5a3f46fdb8c522694b1e1b95527e08812c6db33b
Iron-Baloou <marco.jan@gmx.de>
2023-07-11 11:33:35 +0200

    init commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
 // import './style.css';
 // import Icon from './icon.png';
-import {createNPCBoard, onclick} from './modules/function_game.js';
+import {createNPCBoard} from './modules/function_game.js';
 let count = 50;
 let count_ships = 16;
 
-const SIZE = 8;
-
-const SHIP_SIZES = [4, 3, 3, 2, 2, 2];
+function init(){
+    createNPCBoard();
+  }
+  
+  init();
--- src/modules/function_game.js
+++ src/modules/function_game.js
@@ -4,112 +4,38 @@
 //import section end
 
 //Function section
+const SIZE = 8;
 
+const SHIP_SIZES = [4, 3, 3, 2, 2, 2];
 // Funktion zum Erstellen des Spielfelds und Platzieren der Schiffe des NPC
 function createNPCBoard() {
     // Spielfeld mit Nullen initialisieren
-    npc_board = new Array(SIZE).fill(0).map(() => new Array(SIZE).fill(0));
-  
+    const npc_board = [];
+
     // Schiffe platzieren
-    for (let size of SHIP_SIZES) {
-      let placed = false;
-      while (!placed) {
-        let x = Math.floor(Math.random() * SIZE);
+    for (let shipSize of SHIP_SIZES) {
+        let ship = [];
+        let direction = Math.floor(Math.random() * 2);
+        let x = Math.floor(Math.random() * (SIZE - shipSize));
         let y = Math.floor(Math.random() * SIZE);
-        let orientation = Math.floor(Math.random() * 2); // 0 = horizontal, 1 = vertikal
-  
-        // Überprüfen, ob das Schiff auf dem Spielfeld platziert werden kann
-        if (orientation === 0 && x + size <= SIZE) {
-          let valid = true;
-          for (let i = 0; i < size; i++) {
-            if (npc_board[x + i][y] !== 0) {
-              valid = false;
-              break;
+        for (let i = 0; i < shipSize; i++) {
+            if (direction === 0) {
+                ship.push([x + i, y]);
+            } else {
+                ship.push([x, y + i]);
             }
-          }
-          if (valid) {
-            for (let i = 0; i < size; i++) {
-              npc_board[x + i][y] = size;
-            }
-            npc_ships.push({ size, x, y, orientation });
-            placed = true;
-          }
-        } else if (orientation === 1 && y + size <= SIZE) {
-          let valid = true;
-          for (let i = 0; i < size; i++) {
-            if (npc_board[x][y + i] !== 0) {
-              valid = false;
-              break;
-            }
-          }
-          if (valid) {
-            for (let i = 0; i < size; i++) {
-              npc_board[x][y + i] = size;
-            }
-            npc_ships.push({ size, x, y, orientation });
-            placed = true;
-          }
         }
-      }
+        npc_board.push(ship);
     }
-  }
-  function onClick(event) {
-    let rect = canvas.getBoundingClientRect();
-    let x = event.clientX - rect.left;
-    let y = event.clientY - rect.top;
-  
-    let fieldX = Math.floor(x / 50); // Berechne die Spalte im Raster
-    let fieldY = Math.floor(y / 50); // Berechne die Zeile im Raster
-  
-    // Überprüfe, ob das Kästchen innerhalb des Rasters liegt
-    if (fieldX >= 0 && fieldX < SIZE && fieldY >= 0 && fieldY < SIZE) {
-      // Überprüfe, ob ein Schiff getroffen wurde
-      if (npc_board[fieldY][fieldX] === 0) {
-        npc_board[fieldY][fieldX] = 'X'; // Setze den Wert im npc_board Array auf "X", um anzuzeigen, dass das Feld schon getroffen wurde
-      } else if (npc_board[fieldY][fieldX] === 'X' || npc_board[fieldY][fieldX] === 'Y') {
-        if (npc_board[fieldY][fieldX] === 'Y') {
-          alert('Du hast schon hier geschossen');
-        } else if (npc_board[fieldY][fieldX] === 'X') {
-          alert('Du hast schon hier geschossen');
-        }
-      } else {
-        npc_board[fieldY][fieldX] = 'Y'; // Setze 'Y' in ein getroffenes Schiff, um es zu markieren
-        console.log(`Treffer  Spalte: ${fieldX} Zeile: ${fieldY}`);
-        count_ships--;
-        console.log(count_ships);
-      }
-  
-      context.fillStyle = npc_board[fieldY][fieldX] === 'Y' ? 'green' : 'red';
-      context.fillRect(fieldX * 50, fieldY * 50, 50, 50);
-  
-      if (count_ships === 0) {
-        alert('Du hast gewonnen');
-        document.removeEventListener('click', onClick);
-      } else {
-        count--;
-        alert(`Du hast noch ${count} Versuche`);
-        if (count === 0) {
-          alert('Du hast verloren');
-          document.removeEventListener('click', onClick);
-        }
-      }
-  
-      // Printe das neue Spielfeld in die Konsole
-      for (let line of npc_board) {
-        console.log(line.join(' '));
-      }
-    }
+    console.log(npc_board);
+
   }
 
-  function init(){
-    createNPCBoard();
-    drawGrid();
-  }
-  
-  init();
+
+ 
   
   //expport section
 
-    export {createNPCBoard, onClick};
+    export {createNPCBoard};
 
     //export section end
