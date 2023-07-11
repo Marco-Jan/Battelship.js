b7a1d292195dd7934f72d68b3f31ff356928f855
Iron-Baloou <marco.jan@gmx.de>
2023-07-11 12:32:16 +0200

    init commit

--- src/modules/function_game.js
+++ src/modules/function_game.js
@@ -4,7 +4,6 @@
 //import section end
 
 //Function section
-//Function section
 const SIZE = 8;
 
 const SHIP_SIZES = [4, 3, 3, 2, 2, 2];
@@ -15,89 +14,63 @@ function createNPCBoard() {
 
     // Schiffe platzieren
     for (let shipSize of SHIP_SIZES) {
-        let shipPlaced = false;
-        let ship;
+        const ship = [];
 
-        while (!shipPlaced) {
-            ship = [];
+        const direction = Math.floor(Math.random() * 2);//0 = horizontal, 1 = vertikal
+        console.log(direction,'direction');
+        console.log(shipSize,'shipSize');
+        const x = Math.floor(Math.random() * (SIZE - shipSize));
+        console.log(x,'x');
+        
+        const y = Math.floor(Math.random() * SIZE);
+        console.log(y,'y');
 
-            const direction = Math.floor(Math.random() * 2); // 0 = horizontal, 1 = vertikal
-            const x = Math.floor(Math.random() * (SIZE - shipSize));
-            const y = Math.floor(Math.random() * SIZE);
-
+        for (let i = 0; i < shipSize; i++) {
             let valid = true;
+            if (direction === 0 && valid === true) {  
+                ship.push([x + i, y]);
+                valid = false
+            } 
+            else if (direction === 1 && valid === true) {
+                ship.push([x, y + i]);
+                valid = false
+            } else {
+                
 
-            for (let i = 0; i < shipSize; i++) {
-                if (direction === 0) {
-                    for (let j = 0; j < npc_board.length; j++) {
-                        for (let k = 0; k < npc_board[j].length; k++) {
-                            if (npc_board[j][k][0] === x + i && npc_board[j][k][1] === y) {
-                                valid = false;
-                                break;
-                            }
-                        }
-                        if (!valid) {
-                            break;
-                        }
-                    }
-                } else if (direction === 1) {
-                    for (let j = 0; j < npc_board.length; j++) {
-                        for (let k = 0; k < npc_board[j].length; k++) {
-                            if (npc_board[j][k][0] === x && npc_board[j][k][1] === y + i) {
-                                valid = false;
-                                break;
-                            }
-                        }
-                        if (!valid) {
-                            break;
-                        }
-                    }
-                }
-
-                if (!valid) {
-                    break;
-                }
-            }
-
-            if (valid) {
-                for (let i = 0; i < shipSize; i++) {
-                    if (direction === 0) {
-                        ship.push([x + i, y]);
-                    } else if (direction === 1) {
-                        ship.push([x, y + i]);
-                    }
-                }
-
-                shipPlaced = true;
-            }
         }
-
-        npc_board.push(ship);
     }
+        npc_board.push(ship);
+        console.log(ship,'pushed ship');
 
+    }
+    console.log(npc_board,'npc_board');
     return npc_board;
+   
 }
+
+
 
 // Funktion zeichnet das NPC-Spielfelds
-function renderNPCBoard(npc_board) {
-    const cells = document.querySelectorAll('#playfield .cell');
+  function renderNPCBoard(npc_board) {
 
-    // Alle Zellen auf den Standardzustand zurücksetzen
-    cells.forEach((cell) => {
-        cell.style.backgroundColor = 'white';
-    });
+  const cells = document.querySelectorAll('#playfield .cell');
 
-    // Gesetzte Schiffe farbig darstellen
-    npc_board.forEach((ship) => {
-        ship.forEach(([x, y]) => {
-            const cellIndex = y * SIZE + x;
-            cells[cellIndex].style.backgroundColor = 'blue';
-        });
-    });
+  // Alle Zellen auf den Standardzustand zurücksetzen
+  cells.forEach((cell) => {
+      cell.style.backgroundColor = 'white';
+  });
+
+  // Gesetzte Schiffe farbig darstellen
+  npc_board.forEach((ship) => {
+      ship.forEach(([x, y]) => {
+          const cellIndex = y * SIZE + x;
+          cells[cellIndex].style.backgroundColor = 'blue';
+      });
+  });
 }
+  
+  //expport section
 
-//expport section
+    export {createNPCBoard,renderNPCBoard};
 
-export { createNPCBoard, renderNPCBoard };
-
-//export section end
+    //export section end
