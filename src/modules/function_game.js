b65dc39817f6f5be6ec4a3eea2ad186a5cea2440
Iron-Baloou <marco.jan@gmx.de>
2023-07-11 13:13:55 +0200

    init commit

--- src/modules/function_game.js
+++ src/modules/function_game.js
@@ -7,70 +7,67 @@
 const SIZE = 8;
 
 const SHIP_SIZES = [4, 3, 3, 2, 2, 2];
-// Funktion zum Erstellen des Spielfelds und Platzieren der Schiffe des NPC
-function createNPCBoard() {
-    // Spielfeld mit Nullen initialisieren
-    const npc_board = [];
+// Klasse zum Erstellen des Spielfelds und Platzieren der Schiffe des NPC
+class CreateNPCBoard {
+    constructor() {
+        this.board = [];
+        this.ships = [];
+        this.createBoard();
+        this.placeShips();
+    }
 
-    // Schiffe platzieren
-    for (let shipSize of SHIP_SIZES) {
-        const ship = [];
-
-        const direction = Math.floor(Math.random() * 2);//0 = horizontal, 1 = vertikal
-        console.log(direction,'direction');
-        console.log(shipSize,'shipSize');
-        const x = Math.floor(Math.random() * (SIZE - shipSize));
-        console.log(x,'x');
-        
-        const y = Math.floor(Math.random() * SIZE);
-        console.log(y,'y');
-
-        for (let i = 0; i < shipSize; i++) {
-            let valid = true;
-            if (direction === 0 && valid === true) {  
-                ship.push([x + i, y]);
-                valid = false
-            } 
-            else if (direction === 1 && valid === true) {
-                ship.push([x, y + i]);
-                valid = false
-            } else {
-                
-
+    createBoard() {
+        for (let i = 0; i < SIZE; i++) {
+            this.board[i] = [];
+            console.log(i,'i');
+            for (let j = 0; j < SIZE; j++) {
+                this.board[i][j] = 0;
+                console.log(j,'j');
+            }
         }
     }
-        npc_board.push(ship);
-        console.log(ship,'pushed ship');
 
+    placeShips() {
+        for (let shipSize of SHIP_SIZES) {
+            let ship = [];
+            let direction = Math.floor(Math.random() * 2);
+            let x = Math.floor(Math.random() * (SIZE - shipSize));
+            let y = Math.floor(Math.random() * SIZE);
+            for (let i = 0; i < shipSize; i++) {
+                if (direction === 0) {
+                    ship.push({ x: x + i, y: y, length: shipSize, valid: false });
+                    console.log(ship,'shipx');
+                } else {
+                    ship.push({ x: x, y: y + i, length: shipSize, valid: false });
+                    console.log(ship,'shipy');
+                }
+            }
+            this.ships.push(ship);
+        }
     }
-    console.log(npc_board,'npc_board');
-    return npc_board;
-   
 }
 
+const npcBoard = new CreateNPCBoard();
+const renderedBoard = renderNPCBoard(npcBoard.ships);
 
+// Funktion zeichnet das NPC-Spielfeld
+function renderNPCBoard(ships) {
+    const cells = document.querySelectorAll('#playfield .cell');
 
-// Funktion zeichnet das NPC-Spielfelds
-  function renderNPCBoard(npc_board) {
+    // Alle Zellen auf den Standardzustand zurücksetzen
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = 'white';
+    });
 
-  const cells = document.querySelectorAll('#playfield .cell');
-
-  // Alle Zellen auf den Standardzustand zurücksetzen
-  cells.forEach((cell) => {
-      cell.style.backgroundColor = 'white';
-  });
-
-  // Gesetzte Schiffe farbig darstellen
-  npc_board.forEach((ship) => {
-      ship.forEach(([x, y]) => {
-          const cellIndex = y * SIZE + x;
-          cells[cellIndex].style.backgroundColor = 'blue';
-      });
-  });
+    // Gesetzte Schiffe farbig darstellen
+    ships.forEach((ship) => {
+        ship.forEach(({ x, y }) => {
+            const cellIndex = y * SIZE + x;
+            cells[cellIndex].style.backgroundColor = 'blue';
+        });
+    });
 }
   
-  //expport section
-
-    export {createNPCBoard,renderNPCBoard};
-
-    //export section end
+//export section
+export { CreateNPCBoard, renderNPCBoard };
+//export section end
